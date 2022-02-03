dd09ffeaddb07add9cab422b796997e278a9720b
Eoinburke <burkeyeoin45@gmail.com>
2022-02-03 22:22:44 +0000

    Add category CRUD operations

--- static/js/script.js
+++ static/js/script.js
@@ -8,6 +8,15 @@ $(document).ready(function () {
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
     $("select").formSelect();
+
+    const modalOkBtn = document.getElementById('modal-ok-btn');
+    $('.remove-btn').click((event) => {
+        const modal = document.getElementById('confirmation-dialog');
+        const instance = M.Modal.init(modal, { dismissible:false });
+        instance.open();
+        modalOkBtn.href = event.currentTarget.href;
+        return false;
+    });
 });
 
 var ingredientField = $(".ingredient").length;
